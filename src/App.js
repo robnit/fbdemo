4820d5da7c52fb4a9dbd810aa88d863f2f005d09
Robin Miller <rmillerpdx@gmail.com>
2019-07-04 10:47:12 -0700

    randomize grid on mount

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { PureComponent } from 'react'
 import Grid from './Grid';
+import Box from './Box';
+import { toggle, generateGrid } from './utils'
 
 import './App.css';
+import { thisExpression } from '@babel/types';
 
-function App() {
-  return (
-    <div className="container mt-5">
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-      <Grid/>
-    </div>
-  );
+class App extends PureComponent {
+  state = { grid: [] };
+
+  componentDidMount() {
+    this.setState({ grid: generateGrid(10) })
+  }
+
+
+  renderRow(boxes) {
+    return (
+      <div className="row justify-content-center">
+        { boxes.map((value) => <Box on={ value } />) }
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="container mt-5">
+        { this.state.grid.map(this.renderRow) }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,37 @@
+export function toggle(row, cell, arr) {
+  if (row > arr.length || (arr[row] && cell > arr[row].length)) return arr;
+
+  const grid = [...arr];
+  function flip(x, y) {
+    if (!grid[x]) {
+      console.log(x, y);
+    }
+    grid[x][y] = !grid[x][y];
+  }
+
+  grid[row][cell] = !grid[row][cell];
+  if (row > 0) flip(row - 1, cell);
+  if (cell > 0) flip(row, cell - 1);
+  if (row < grid.length - 1) {
+    flip(row + 1, cell);
+  }
+  if (cell < grid[row].length - 1) {
+    flip(row, cell + 1);
+  }
+
+  return grid;
+}
+
+export function generateGrid(size) {
+  const row = Array(size).fill(false);
+  let output = Array(size).fill([]);
+  output = output.map(() => row.slice());
+
+  for (let i = 0; i < 200; i += 1) {
+    const randoX = Math.floor(Math.random() * size);
+    const randoY = Math.floor(Math.random() * size);
+    output = toggle(randoX, randoY, output);
+  }
+
+  return output;
+}
